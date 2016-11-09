052d428e0bca897883fd974daecc3122ec3fff24
Igor Proskura <khors.fb@gmail.com>
2016-11-09 08:14:01 +0200

    pg78

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Hello pony racer</h1>
-  <h2>welcome: {{ user.name }}</h2>   
-`
+    <h1>Hello pony racer</h1>
+    <h2>welcome: {{ user.name }}</h2>
+    <ns-races></ns-races>
+  `
 })
 export class PonyRacerAppComponent {
   user: any = {name: 'Cedr1L'};
