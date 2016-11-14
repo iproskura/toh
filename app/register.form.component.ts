0ce70680d6f04dcd2c4102c1990c8a3628decbce
Igor Proskura <khors.fb@gmail.com>
2016-11-14 03:35:18 +0200

    before form groups

--- app/register.form.component.ts
+++ app/register.form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, Form } from '@angular/forms';
 
 class User {
   username: string;
@@ -11,17 +11,26 @@ class User {
   templateUrl: './app/templates/register-form.component.html'
 })
 export class RegisterFormComponent {
+  userForm: FormGroup;
   usernameCtrl: FormControl;
   passwordCtrl: FormControl;
-  userForm: FormGroup;
+  birthdateCtrl: FormControl;
+
+  static isOldEnough(control: FormControl){
+    const birthDatePlus18 = new Date(control.value);
+    birthDatePlus18.setFullYear(birthDatePlus18.getFullYear() + 18);
+    return birthDatePlus18 < new Date() ? null : {tooYoung: true};
+  }
 
   constructor(fb: FormBuilder) {
     this.usernameCtrl = fb.control('', Validators.compose([Validators.required, Validators.minLength(3)]));
     this.passwordCtrl = fb.control('', Validators.required);
+    this.birthdateCtrl = fb.control('', Validators.compose([Validators.required, RegisterFormComponent.isOldEnough]));
 
     this.userForm = fb.group({
       username: this.usernameCtrl,
-      password: this.passwordCtrl
+      password: this.passwordCtrl,
+      birthdate: this.birthdateCtrl
     });
   }
 
@@ -34,3 +43,14 @@ export class RegisterFormComponent {
     console.log(this.userForm.value);
   }
 }
+
+
+/*
+const isOldEnough = (control: FormControl) => {
+  const birthDatePlus18 = new Date(control.value);
+  birthDatePlus18.setFullYear(birthDatePlus18.getFullYear() + 18);
+  return birthDatePlus18 < new Date() ? null : {tooYoung: true};
+};*/
+
+/*this.usernameCtrl = fb.control('', Validators.compose([Validators.required, Validators.minLength(3)]));*/
+
