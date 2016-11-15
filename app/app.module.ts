e220f5962fc7447d5c1697c2cd942be44779f87a
Igor Proskura <is.proskura@gmail.com>
2016-11-15 17:56:00 +0200

    before mult components

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
-import { PonyRacerAppComponent }  from './app.component';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ PonyRacerAppComponent ],
-  bootstrap:    [ PonyRacerAppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
